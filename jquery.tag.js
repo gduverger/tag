6b0b748b3c3fffdb846b8b4c671060d4e7ca0150
Georges Duverger <georges.duverger@gmail.com>
2010-12-20 17:32:30 -0500

    Array.indexOf bug fix for IE

--- jquery.tag.js
+++ jquery.tag.js
@@ -222,7 +222,6 @@
 				$(this).hide();
 				doneEditing( selector, tags, settings );
 			});
-			// bindDoneEditing( selector, settings );
 		} else {
 			doneEditingElement.show();
 		}
@@ -252,7 +251,7 @@
 			if ( this.inputField ) { $(this).saveTag( tags, settings ); }
 			$(this.removeLink).hide();
 			$(this).removeClass( settings.editModeClass ); // EDIT MODE
-		})
+		});
 		
 		// Restore the original "Edit tags" link
 		$(settings.editTagsSelector).show();
@@ -280,3 +279,16 @@
 	};
 	
 })( jQuery );
+
+// To fix the fact that indexOf is 'undefined' in IE
+// http://soledadpenades.com/2007/05/17/arrayindexof-in-internet-explorer/
+if(!Array.indexOf){
+	Array.prototype.indexOf = function(obj){
+		for(var i=0; i<this.length; i++){
+			if(this[i]==obj){
+				return i;
+			}
+		}
+		return -1;
+	}
+}
